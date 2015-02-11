41fc3f0db150dfdc7324b4e1a8e6b8beb06428fb
Kong Jirawong <greannetwork@gmail.com>
2015-02-11 10:54:19 +0700

    change mock framework from easymock to mockito

--- src/test/java/com/mockupcode/slack/rtm/api/SlackAuthenTest.java
+++ src/test/java/com/mockupcode/slack/rtm/api/SlackAuthenTest.java
@@ -11,7 +11,7 @@ import java.io.InputStream;
 import org.apache.commons.httpclient.HttpClient;
 import org.apache.commons.httpclient.HttpStatus;
 import org.apache.commons.httpclient.methods.GetMethod;
-import org.easymock.EasyMock;
+//import org.easymock.EasyMock;
 import static org.hamcrest.core.Is.*;
 import org.junit.After;
 import org.junit.AfterClass;
@@ -20,7 +20,9 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.powermock.api.easymock.PowerMock;
+import org.mockito.Mockito;
+import org.powermock.api.mockito.PowerMockito;
+//import org.powermock.api.easymock.PowerMock;
 import org.powermock.core.classloader.annotations.PowerMockIgnore;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
@@ -53,22 +55,20 @@ public class SlackAuthenTest {
     public void tearDown() {
     }
 
+
     @Test
     public void testTokenAuthenWithInvalidToken() throws Exception {
         InputStream inputStream = new ByteArrayInputStream("{\"ok\":false}".getBytes());
-
-        HttpClient httpClient = PowerMock.createMock(HttpClient.class);
-        GetMethod getMethod = PowerMock.createMock(GetMethod.class);
-
-        PowerMock.expectNew(HttpClient.class).andReturn(httpClient);
-        PowerMock.expectNew(GetMethod.class, "https://slack.com/api/rtm.start?token=token").andReturn(getMethod);
-
-        EasyMock.expect(httpClient.executeMethod(getMethod)).andReturn(HttpStatus.SC_OK);
-        EasyMock.expect(getMethod.getResponseBodyAsStream()).andReturn(inputStream);
-        getMethod.releaseConnection();
-
-        PowerMock.replay(httpClient, HttpClient.class, getMethod, GetMethod.class);
-
+        
+        HttpClient httpClient = PowerMockito.mock(HttpClient.class);
+        GetMethod getMethod = PowerMockito.mock(GetMethod.class);
+        
+        PowerMockito.whenNew(HttpClient.class).withNoArguments().thenReturn(httpClient);
+        PowerMockito.whenNew(GetMethod.class).withAnyArguments().thenReturn(getMethod);
+        
+        Mockito.when(httpClient.executeMethod(getMethod)).thenReturn(HttpStatus.SC_OK);
+        Mockito.when(getMethod.getResponseBodyAsStream()).thenReturn(inputStream);
+        
         SlackInfo tokenAuthen = new SlackAuthen().tokenAuthen("token",null,0);
         assertThat(tokenAuthen.isOk(), is(false));
     }
@@ -76,21 +76,17 @@ public class SlackAuthenTest {
     @Test
     public void testTokenAuthenSuccess() throws Exception {
         InputStream inputStream = new ByteArrayInputStream("{\"ok\":true}".getBytes());
-
-        HttpClient httpClient = PowerMock.createMock(HttpClient.class);
-        GetMethod getMethod = PowerMock.createMock(GetMethod.class);
-
-        PowerMock.expectNew(HttpClient.class).andReturn(httpClient);
-        PowerMock.expectNew(GetMethod.class, "https://slack.com/api/rtm.start?token=token").andReturn(getMethod);
-
-        EasyMock.expect(httpClient.executeMethod(getMethod)).andReturn(HttpStatus.SC_OK);
-        EasyMock.expect(getMethod.getResponseBodyAsStream()).andReturn(inputStream);
-        getMethod.releaseConnection();
-
-        PowerMock.replay(httpClient, HttpClient.class, getMethod, GetMethod.class);
-
+        
+        HttpClient httpClient = PowerMockito.mock(HttpClient.class);
+        GetMethod getMethod = PowerMockito.mock(GetMethod.class);
+        
+        PowerMockito.whenNew(HttpClient.class).withNoArguments().thenReturn(httpClient);
+        PowerMockito.whenNew(GetMethod.class).withAnyArguments().thenReturn(getMethod);
+        
+        Mockito.when(httpClient.executeMethod(getMethod)).thenReturn(HttpStatus.SC_OK);
+        Mockito.when(getMethod.getResponseBodyAsStream()).thenReturn(inputStream);
+        
         SlackInfo tokenAuthen = new SlackAuthen().tokenAuthen("token",null,0);
         assertThat(tokenAuthen.isOk(), is(true));
     }
-
 }
